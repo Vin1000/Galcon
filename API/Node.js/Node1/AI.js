715c13de5a067ee4850a05153cead8b2ebf4cb34
Marc-Antoine Babin <mababin93@gmail.com>
2015-07-11 09:44:44 -0400

    first commit of API

---
+++ API/Node.js/Node1/AI.js
@@ -0,0 +1,22 @@
+﻿// Set your team name here!!!
+var name = "Bot";
+var game;
+
+var set_game = function (obj) {
+  game = obj;
+}
+
+var update = function (game_objects) {
+  console.log("update");
+};
+
+var set_name = function () {
+  console.log("set_name");
+  game.set_name(name);
+};
+
+module.exports = {
+  set_name: set_name,
+  update: update,
+  set_game: set_game
+};
